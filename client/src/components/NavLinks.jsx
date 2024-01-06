226f47fd9847d06f8d4469427653a7a870fea4d9
iamnitesh108 <iamnitesh108@gmail.com>
2024-01-06 07:23:46 +0530

    first commit

---
+++ client/src/components/NavLinks.jsx
@@ -0,0 +1,33 @@
+import { NavLink } from "react-router-dom";
+import { useDashboardContext } from "../pages/DashboardLayout";
+import links from "../utils/links";
+
+const NavLinks = ({isBigSideBar}) => {
+  const { toggleSidebar, user } = useDashboardContext();
+
+  return (
+    <div className="nav-links">
+      {links.map((link) => {
+        const { text, path, icon } = link;
+        const { role } = user;
+        if (role !== 'admin' && path === 'admin') return;
+
+
+        return (
+          <NavLink
+            to={path}
+            key={text}
+            className="nav-link"
+            onClick={isBigSideBar ? null : toggleSidebar}
+            end
+          >
+            <span className="icon">{icon}</span>
+            {text}
+          </NavLink>
+        );
+      })}
+    </div>
+  );
+};
+
+export default NavLinks;
